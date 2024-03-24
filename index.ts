adacc5b866364351bdd14ab72749a7121822812a
yamman <sheikhrameez031@gmail.com>
2024-03-25 02:50:29 +0500

    I've created a simple CLI calculator Using TypeScript, Node.js and Inquirer

---
+++ index.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "input", name: "firstNumber" },
+  { message: "Enter Second number", type: "input", name: "secondNumber" },
+  {
+    message: "Select any operator for perform Calculation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Substraction", "Multiplication", "Division"],
+  },
+]);
+
+const firstNumber = parseFloat(answer.firstNumber);
+const secondNumber = parseFloat(answer.secondNumber);
+
+if (answer.operator === "Addition") {
+  console.log(`${answer.firstNumber}` + `${answer.secondNumber}`);
+} else if (answer.operator === "Substraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+}
+else{
+  console.log('Please select valid operator');
+}
